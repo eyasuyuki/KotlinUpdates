1e18290df55aed61b95f7a57e96a25e0a1b12539
Yasuyuki ENDO <eyasuyuki@gmail.com>
2019-08-24 01:05:41 +0900

    add smartGreeting/nonSmartGreeting

--- src/main/kotlin/org/javaopen/kotlin/updates/Animal.kt
+++ src/main/kotlin/org/javaopen/kotlin/updates/Animal.kt
@@ -20,3 +20,17 @@ fun action(animal: Animal) {
         is Dog -> animal.bark() // Dogであることは自明なのでDogのメソッドが呼べる
     }
 }
+
+// before Kotlin 1.2
+fun nonSmartGreeting(name: String?) {
+    if (name != null && name.isNotBlank()) {
+        println("Hello, ${name.capitalize()}")
+    }
+}
+
+// after Kotlin 1.3
+fun smartGreeting(name: String?) {
+    if (!name.isNullOrBlank()) {
+        println("Hello, ${name.capitalize()}")
+    }
+}
